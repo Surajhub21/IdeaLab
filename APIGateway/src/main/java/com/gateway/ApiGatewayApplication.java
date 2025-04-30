5dab26a145317e6076b14a4b4252fe1ed258f535
Suraj Mondal <sm.surojmondal@gmail.com>
2025-04-30 06:43:33 +0530

    Finish GateWay

--- APIGateway/src/main/java/com/gateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/gateway/ApiGatewayApplication.java
@@ -2,12 +2,32 @@ package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("USERSERVICE", r-> r.path("/user/**" , "/public/**" , "/email/**")
+						.filters(f -> f.stripPrefix(0))
+						.uri("lb://USERSERVICE"))
+				.route("PESTSERVICE", r-> r.path("/pests/**")
+						.filters(f -> f.stripPrefix(0))
+						.uri("lb://PESTSERVICE"))
+				.route("BOOKINGSERVICE", r-> r.path("/booking/**")
+						.filters(f -> f.stripPrefix(0))
+						.uri("lb://BOOKINGSERVICE"))
+				.build();
 	}
 
 }
--- UserService/src/main/java/com/userservice/controller/PublicController.java
+++ UserService/src/main/java/com/userservice/controller/PublicController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/public")
 @Slf4j
 public class PublicController {
 
